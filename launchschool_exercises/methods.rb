be9380bdb783ea29efd68ac37d2bd27088d9af88
Josh Cook <jcook2303@gmail.com>
2023-03-01 08:36:46 -0800

    Methods lessons from Odin Project and Launchschool

---
+++ launchschool_exercises/methods.rb
@@ -0,0 +1,18 @@
+# https://launchschool.com/books/ruby/read/methods
+
+=begin
+# #1
+def greeting(name)
+    "Hello, #{name}! Welcome!"
+end
+
+puts greeting("Josh")
+=end
+
+
+# #3
+def multiply(num_one, num_two)
+    num_one * num_two
+end
+
+puts multiply(5, 2)
