ca8de408302e49f6914936bc6c9ee0ce3d7560fd
Josh Cook <jcook2303@gmail.com>
2023-03-05 11:22:16 -0800

    Caesar cipher finished project.

--- projects/cipher.rb
+++ projects/cipher.rb
@@ -8,19 +8,26 @@ LOWER_CASE = *("a".."z")
 
 def caesar_cipher(string, rotation)
     new_array = []
-    holding_array = string.split("")
-    holding_array.each_with_index do |char, index, array|
+    string.split("") do |char|
         if UPPER_CASE.include?(char) 
-            puts "#{char} has Index of #{UPPER_CASE.index(char)}"
-            new_array.push(UPPER_CASE.at(index + rotation))
-            puts index
+            upper_array_index = UPPER_CASE.index(char)
+            if upper_array_index + rotation > 25
+                new_index = (upper_array_index + rotation) - 25
+                new_array.push(UPPER_CASE.at(new_index - 1))
+            end
+            new_array.push(UPPER_CASE.at(upper_array_index + rotation))
         elsif LOWER_CASE.include?(char)
-            puts "#{char} has Index of #{LOWER_CASE.index(char)}"
-            new_array.push(LOWER_CASE.at(index + rotation))
-            puts index
+            lower_array_index = LOWER_CASE.index(char)
+            if lower_array_index + rotation > 25
+                new_index = (lower_array_index + rotation) - 25
+                new_array.push(LOWER_CASE.at(new_index - 1))
+            end            
+            new_array.push(LOWER_CASE.at(lower_array_index + rotation))
+        else
+            new_array.push(char)
         end
     end
-    puts new_array
+    puts "Ciphered String: #{new_array.join}"
 end
 
 caesar_cipher(string, rotation)
