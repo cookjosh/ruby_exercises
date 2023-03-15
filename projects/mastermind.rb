5b2fbc33895a765e87a1ccc6340c7e1155d480ce
Josh Cook <jcook2303@gmail.com>
2023-03-14 21:39:32 -0700

    Very minor changes for today. Need a break :)

--- projects/mastermind.rb
+++ projects/mastermind.rb
@@ -1,6 +1,10 @@
 # feedback_array: red means element and index match, black just means match
 # If redoing, I'd probably build all guess/code creation/code generation into a Code module
 
+# 3/13 - Next step, if user is Mastermind, start with computer generating a secret code
+# check that guess and get feedback
+# next step, retain matches and generate remaining elements in array
+
 class GameBoard
   attr_reader :secret_code
   COLORS = ['red', 'blue', 'orange', 'yellow', 'green', 'purple']
@@ -37,6 +41,7 @@ class Player
 end
 
 class GameFlow
+  attr_reader :user_answer
   def initialize(game_board, player)
     @game_board = game_board
     @player = player
@@ -50,16 +55,20 @@ class GameFlow
   end
   def create_environment
     ask_player()
-    puts @user_answer
     if @user_answer == 'mastermind'
       @player.player_create_code()
-      puts @player.player_code
+      return @player.player_code
     elsif @user_answer == 'codebreaker'
       @game_board.generate_secret_code() 
-      puts @game_board.secret_code
+      return @game_board.secret_code
     else
       puts 'Please enter Mastermind or Codebreaker.'
     end
+  end
+  def game_play # Want to write logic of checking arrays against each other
+    # possibly remove `create_environment` func and put in this
+    # conditionals expanded to handle comparing arrays
+    
   end
   def check_guess
     index = 0
@@ -75,7 +84,7 @@ class GameFlow
       end
       index += 1
     end
-    p @feedback_array
+    return @feedback_array
   end
 end
 
@@ -83,5 +92,5 @@ game_board = GameBoard.new
 player = Player.new
 game_flow  = GameFlow.new(game_board, player)
 game_flow.create_environment()
-
+p game_flow.game_play()
 
