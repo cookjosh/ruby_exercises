62992fab335af3d0d9269de5b9070712dbf10498
Josh Cook <jcook2303@gmail.com>
2023-03-17 21:31:47 -0700

    A little progress on tutorial

--- projects/event_manager/lib/event_manager.rb
+++ projects/event_manager/lib/event_manager.rb
@@ -1,6 +1,34 @@
+# Example if Ruby didn't have native CSV parser
+=begin
+
 puts 'Event Manager Initialized!'
 
 lines = File.readlines('event_attendees.csv')
-lines.each do |line|
-  puts line
+lines.each_with_index do |line, index|
+  next if index == 0
+  columns = line.split(",")
+  name = columns[2]
+  puts name
+end
+=end
+
+require 'csv'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+puts 'EventManager initialized'
+
+contents = CSV.open('event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  puts "#{name} #{zipcode}"
 end
