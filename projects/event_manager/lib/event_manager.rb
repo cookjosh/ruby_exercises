45d671238288d0b7bb38c16a0668b171342d14e5
Josh Cook <jcook2303@gmail.com>
2023-03-18 22:02:38 -0700

    Iteration learning API interaction in Ruby

--- projects/event_manager/lib/event_manager.rb
+++ projects/event_manager/lib/event_manager.rb
@@ -13,14 +13,35 @@ end
 =end
 
 require 'csv'
+require 'google/apis/civicinfo_v2'
+
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
-puts 'EventManager initialized'
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
-contents = CSV.open('event_attendees.csv',
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+    legislator_names = legislators.map(&:name)
+    legislator_names.join(", ")
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+puts 'EventManager initialized.'
+
+contents = CSV.open(
+  'event_attendees.csv',
   headers: true,
   header_converters: :symbol
 )
@@ -30,5 +51,7 @@ contents.each do |row|
 
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  legislators = legislators_by_zipcode(zipcode)
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
