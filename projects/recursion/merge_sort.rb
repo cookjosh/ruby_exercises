f2225f695a43a1941878a9e96ba680a9a05440c2
Josh Cook <jcook2303@gmail.com>
2023-03-24 14:32:01 -0700

    Comments added

--- projects/recursion/merge_sort.rb
+++ projects/recursion/merge_sort.rb
@@ -1,11 +1,16 @@
 # Build a method #merge_sort that takes in an array and returns a sorted array, using a recursive merge sort methodology.
+# Needed a little boost to get through the merge logic
+# Followed along with logic of this user: https://github.com/Ganthology/ruby_recursion/tree/main
+
+# Additionally kept getting SystemStackError for depth
+# because I was using .. for arr_one when I needed ...
 
 def merge_sort(arr)
   if arr.length < 2
     return arr
   else
     mid_point = arr.length / 2
-    
+
     arr_one = arr[0...mid_point]
     arr_two = arr[mid_point..arr.length]
     left = merge_sort(arr_one)
